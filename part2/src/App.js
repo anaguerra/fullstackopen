dc20b87f338f0fed9c7d799d2d4acf9dde525244
Ana Guerra <anamaria.guerra@icontainers.com>
2023-03-03 12:11:03 +0000

    renderizar listas

--- part2/src/App.js
+++ part2/src/App.js
@@ -1,35 +1,72 @@
 import './App.css';
 import { Note } from './Note';
-
-const notes = [
-  {
-    id: 1,
-    content: 'HTML is easy',
-    date: '2019-05-30T17:30:31.098Z',
-    important: true,
-    categories: ['sports', 'hobbies']
-  },
-  {
-    id: 2,
-    content: 'Browser can execute only JavaScript',
-    date: '2019-05-30T18:39:34.091Z',
-    important: false
-  },
-  {
-    id: 8,
-    content: 'GET and POST are the most important methods of HTTP protocol',
-    date: '2019-05-30T19:20:14.298Z',
-    important: true
-  },
-]
+import { useState } from 'react';
 
 
 
-const App = () => {
+const App = (props) => {
+  
+  // notes: el estado con todas las notas 
+  // setNotes: la forma de actualizar las notas
+  // el estado inicial se hace con lo que llega por las props
+  const [notes, setNotes] = useState(props.notes);
+  const [newNote, setNewNote] = useState('');
+  const [showAll, setShowAll] = useState(true);
+
+  const handleChange = (event) => {
+    setNewNote(event.target.value);
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const noteAddToState = {
+      id: notes.length+1,
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() < 0.5
+
+    }
+    // ojo, siempre array nuevo, no mutar el original
+    setNotes([...notes, noteAddToState]);
+    setNewNote('');
+  }
+
+  const handleShowAll = () => {
+    setShowAll(() => !showAll);
+  }
+ 
+  //<--el último botón del formulario funciona como submit-->
   return (
-    <ul>
-      {notes.map(note => <Note key={note.id} {...note} />)}
-    </ul>
+    <div>
+      <h1>Notes</h1>
+      <button onClick={handleShowAll}>
+        { showAll ? 'Show only important': 'Show all'}
+      </button>
+      
+      <ul>
+        {notes
+          .filter((note) => {
+            if (showAll) return true;
+            return note.important === true
+          })
+          .map((note) => (
+            <Note key={note.id} {...note} />
+          ))
+        
+          // otra forma de hacerlo
+          //.map((note) => <Note key={note.id} {...note} />)}
+
+        }
+
+        
+
+      </ul>
+      <form onSubmit={handleSubmit}>
+        <input type='text' onChange={handleChange} value={newNote} />
+        <button>Crear nota</button>
+      </form>
+    </div>
   );
 }
 
